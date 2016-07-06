18014f98c78b1e8b7a54f600ec9c88a536dcc28a
Emil Hedemalm <emil_hedemalm@hotmail.com>
2016-07-06 17:43:16 +0200

    First commit in new repo

---
+++ EntityStates/EntityStates.h
@@ -0,0 +1,13 @@
+// Emil Hedemalm
+// 2013-06-30
+
+/// States for the RuneRPG!
+namespace RuneEntityPropertyState {
+enum entityStates{
+	NULL,
+	PLAYER,
+	NPC,
+	FOE,
+	TRIGGER,
+	MAX_ENTITY_STATES,
+};};
---
+++ RuneDirectories.h
@@ -0,0 +1,8 @@
+// Emil Hedemalm
+// 2013-07-24
+
+// Because it's confusing to have it separately in each game state or manager.
+#ifndef RUNE_DIRECTORIES_H
+#define RUNE_DIRECTORIES_H
+
+#endif
